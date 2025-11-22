c464039139ac8ecf186ab703434c08f0e2ef2fff
blur <thetobiterra@gmail.com>
2025-11-22 17:48:35 +0100

    stripe webhook

---
+++ app/api/webhooks/stripe/route.ts
@@ -0,0 +1,28 @@
+import { NextRequest, NextResponse } from "next/server";
+import Stripe from "stripe";
+import { updateOrderToPaid } from "@/lib/actions/order.actions";
+
+export async function POST(req: NextRequest) {
+  const event = await Stripe.webhooks.constructEvent(
+    await req.text(),
+    req.headers.get("stripe-signature") as string,
+    process.env.STRIPE_WEBHOOK_SECRET as string
+  );
+  if (event.type === "charge.succeeded") {
+    const { object } = event.data;
+    // Update order status
+    await updateOrderToPaid({
+      orderId: object.metadata.orderId,
+      paymentResult: {
+        id: object.id,
+        status: "COMPLETED",
+        email_address: object.billing_details.email as string,
+        pricePaid: (object.amount / 100).toFixed(),
+      },
+    });
+    return NextResponse.json({ message: "updateOrderToPaid was successfull" });
+  }
+  return NextResponse.json({
+    message: "event is not charge.succeeded successfull",
+  });
+}
